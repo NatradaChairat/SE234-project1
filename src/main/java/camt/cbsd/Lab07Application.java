8a7f70e70a6e7d13ab8e93f397ef5b543e453360
GL552\GL552J <natrada_chairat@cmu.ac.th>
2017-05-14 05:28:16 +0700

    Test method getCousreCount() in the CoruseServiceImpl

--- src/main/java/camt/cbsd/Lab07Application.java
+++ src/main/java/camt/cbsd/Lab07Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Lab07Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Lab07Application.class, args);
 	}
 }
--- src/test/java/camt/cbsd/Lab05ApplicationTests.java
+++ src/test/java/camt/cbsd/Lab05ApplicationTests.java
@@ -1,9 +1,18 @@
 package camt.cbsd;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,5 +21,28 @@ public class Lab05ApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+	public void testGetCourseCount(){
+
+		/*CourseServiceImpl course = new CourseServiceImpl();
+		assert(course.getCourseCount()== 3);*/
+		WebDriver driver = new FirefoxDriver();
+		String baseUrl = "http://localhost:4200";
+        
+        //assertEquals("Enroll Course 3", driver.findElement(By.cssSelector("button.btn.btn-primary")).getText());
+        driver.get(baseUrl + "/login");
+        driver.findElement(By.id("username")).clear();
+        driver.findElement(By.id("username")).sendKeys("admin");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("admin");
+        driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button.btn.btn-primary")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.btn.btn-primary")));
+        assertEquals("Enroll Course 3", driver.findElement(By.cssSelector("button.btn.btn-primary")).getText());
+
+	}
+
 
 }
