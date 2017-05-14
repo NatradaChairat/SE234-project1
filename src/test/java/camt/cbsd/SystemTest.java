bfcdc83fb48b1451e0b3a9e7b264df9a96e9d3d5
GL552\GL552J <natrada_chairat@cmu.ac.th>
2017-05-14 17:58:39 +0700

    System test: test login success in firefox and chrome

--- src/test/java/camt/cbsd/SystemTest.java
+++ src/test/java/camt/cbsd/SystemTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -127,7 +128,59 @@ public class SystemTest {
     }
 
     @Test
-    public void testLoginSuccess(){
+    public void testLoginSuccess() throws InterruptedException {
+
+      //  WebDriver driver = new FirefoxDriver();
+        String baseUrl = "http://localhost:4200";
+       /* driver.get(baseUrl + "/login");
+        driver.findElement(By.id("username")).sendKeys("admin");
+        driver.findElement(By.id("password")).sendKeys("admin");
+        driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Course list".equals(driver.findElement(By.linkText("Course list")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        assertEquals("Course list", driver.findElement(By.linkText("Course list")).getText());
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("List".equals(driver.findElement(By.linkText("List")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        assertEquals("List", driver.findElement(By.linkText("List")).getText());
+        assertEquals("Add", driver.findElement(By.linkText("Add")).getText());
+        assertEquals("View", driver.findElement(By.linkText("View")).getText());
+        assertEquals("Add Course", driver.findElement(By.linkText("Add Course")).getText());
+
+*/
+
+        System.setProperty("webdriver.chrome.driver", "E://chromedriver.exe");
+        WebDriver driver2 = new ChromeDriver();
+        driver2.get(baseUrl + "/login");
+        driver2.findElement(By.id("username")).sendKeys("admin");
+        driver2.findElement(By.id("password")).sendKeys("admin");
+        driver2.findElement(By.cssSelector("button.btn.btn-primary")).click();
+
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Course list".equals(driver2.findElement(By.linkText("Course list")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        assertEquals("Course list", driver2.findElement(By.linkText("Course list")).getText());
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("List".equals(driver2.findElement(By.linkText("List")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        assertEquals("List", driver2.findElement(By.linkText("List")).getText());
+        assertEquals("Add", driver2.findElement(By.linkText("Add")).getText());
+        assertEquals("View", driver2.findElement(By.linkText("View")).getText());
+        assertEquals("Add Course", driver2.findElement(By.linkText("Add Course")).getText());
+
 
     }
 }
