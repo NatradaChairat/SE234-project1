90d172b63311d910a87b31dd32a66be9acf5814e
GL552\GL552J <natrada_chairat@cmu.ac.th>
2017-05-14 17:00:05 +0700

    System test: test fail login

--- src/test/java/camt/cbsd/SystemTest.java
+++ src/test/java/camt/cbsd/SystemTest.java
@@ -33,7 +33,101 @@ public class SystemTest {
     }
 
     @Test
-    public void testLogin(){
+    public void testLogin() throws InterruptedException{
+
+        WebDriver driver = new FirefoxDriver();
+        String baseUrl = "http://localhost:4200";
+        driver.get(baseUrl + "/login");
+        // ERROR: Caught exception [unknown command [forXml]]
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Login".equals(driver.findElement(By.cssSelector("h2")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        driver.findElement(By.id("username")).sendKeys("admin");
+        driver.findElement(By.id("password")).sendKeys("1234");
+        driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Unauthorized".equals(driver.findElement(By.cssSelector("div.alert.alert-error")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        assertEquals("Unauthorized", driver.findElement(By.cssSelector("div.alert.alert-error")).getText());
+        // ERROR: Caught exception [unknown command [endForXml]]
+
+
+        driver.get(baseUrl + "/login");
+        // ERROR: Caught exception [unknown command [forXml]]
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Login".equals(driver.findElement(By.cssSelector("h2")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        driver.findElement(By.id("username")).sendKeys("admin");
+        driver.findElement(By.id("password")).sendKeys("admin");
+        driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Enroll Course 3".equals(driver.findElement(By.cssSelector("button.btn.btn-primary")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        assertEquals("Enroll Course 3", driver.findElement(By.cssSelector("button.btn.btn-primary")).getText());
+        // ERROR: Caught exception [unknown command [endForXml]]
+
+
+
+        driver.get(baseUrl + "/login");
+        // ERROR: Caught exception [unknown command [forXml]]
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Login".equals(driver.findElement(By.cssSelector("h2")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        driver.findElement(By.id("username")).sendKeys("");
+        driver.findElement(By.id("password")).sendKeys("admin");
+        driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Username is required".equals(driver.findElement(By.cssSelector("span.help-block")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        assertEquals("Username is required", driver.findElement(By.cssSelector("span.help-block")).getText());
+        // ERROR: Caught exception [unknown command [endForXml]]
+
+
+
+        driver.get(baseUrl + "/login");
+        // ERROR: Caught exception [unknown command [forXml]]
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Login".equals(driver.findElement(By.cssSelector("h2")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+
+        }
+
+        driver.findElement(By.id("username")).sendKeys("admin");
+        driver.findElement(By.id("password")).sendKeys("");
+        driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Password is required".equals(driver.findElement(By.cssSelector("span.help-block")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        assertEquals("Password is required", driver.findElement(By.cssSelector("span.help-block")).getText());
+        // ERROR: Caught exception [unknown command [endForXml]]
+
+
+    }
+
+    @Test
+    public void testLoginSuccess(){
 
     }
 }
