564d878b04db7ef4ee715366666989bfc33cbc5e
GL552\GL552J <natrada_chairat@cmu.ac.th>
2017-05-14 16:01:42 +0700

    System test : test link

--- src/test/java/camt/cbsd/Lab05ApplicationTests.java
+++ src/test/java/camt/cbsd/Lab05ApplicationTests.java
@@ -1,6 +1,12 @@
 package camt.cbsd;
 
 
+import camt.cbsd.config.DataLoader;
+import camt.cbsd.dao.CourseDaoImpl;
+import camt.cbsd.repository.CourseRepository;
+import camt.cbsd.services.CourseService;
+import camt.cbsd.services.CourseServiceImpl;
+import org.aspectj.lang.annotation.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
@@ -12,26 +18,28 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Java6Assertions.fail;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Lab05ApplicationTests {
-
+    CourseService courseService;
 	@Test
 	public void contextLoads() {
 	}
 
+
 	@Test
 	public void testGetCourseCount(){
 
-		/*CourseServiceImpl course = new CourseServiceImpl();
+        /*CourseServiceImpl course = new CourseServiceImpl();
 		assert(course.getCourseCount()== 3);*/
-		WebDriver driver = new FirefoxDriver();
-		String baseUrl = "http://localhost:4200";
-        
+		/*WebDriver driver = new FirefoxDriver();
+		String baseUrl = "http://localhost:4200";*/
+
         //assertEquals("Enroll Course 3", driver.findElement(By.cssSelector("button.btn.btn-primary")).getText());
-        driver.get(baseUrl + "/login");
+        /*driver.get(baseUrl + "/login");
         driver.findElement(By.id("username")).clear();
         driver.findElement(By.id("username")).sendKeys("admin");
         driver.findElement(By.id("password")).clear();
@@ -41,8 +49,27 @@ public class Lab05ApplicationTests {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button.btn.btn-primary")));
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.btn.btn-primary")));
         assertEquals("Enroll Course 3", driver.findElement(By.cssSelector("button.btn.btn-primary")).getText());
+*/
+
+
 
 	}
+
+	@Test
+	public void testGetUserName(){
+        /*WebDriver driver = new FirefoxDriver();
+        String baseUrl = "http://localhost:4200";
+        driver.get(baseUrl + "/list");
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("MITSUHA");
+        driver.findElement(By.cssSelector("button.btn.btn-default")).click();
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p.student-name")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("p.student-name")));
+        assertEquals("MITSUHA Miyamizu", driver.findElement(By.cssSelector("p.student-name")).getText());*/
+
+    }
+
+
 
 
 }
---
+++ src/test/java/camt/cbsd/SystemTest.java
@@ -0,0 +1,39 @@
+package camt.cbsd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Java6Assertions.fail;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by GL552J on 14/5/2560.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SystemTest {
+
+    @Test
+    public void testLink() throws InterruptedException {
+        WebDriver driver = new FirefoxDriver();
+        String baseUrl = "http://localhost:4200";
+        driver.get(baseUrl + "/lp");
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("The resource you have asked is not in the server".equals(driver.findElement(By.cssSelector("h2")).getText())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+        assertEquals("The resource you have asked is not in the server", driver.findElement(By.cssSelector("h2")).getText());
+
+    }
+
+    @Test
+    public void testLogin(){
+
+    }
+}
